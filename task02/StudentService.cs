422b29c9fd8a23e9cf1b8d57250e845c12997b81
akune08 <orlovsky_platon@mail.ru>
2026-07-11 18:18:27 +0600

    Update StudentService.cs

--- task02/StudentService.cs
+++ task02/StudentService.cs
@@ -13,22 +13,17 @@ public class StudentService
 
     public StudentService(List<Student> students) => _students = students;
 
-    // 1. Возвращает студентов указанного факультета
     public IEnumerable<Student> GetStudentsByFaculty(string faculty)
     => _students.Where(student => student.Faculty.ToLower() == faculty.ToLower());
 
-    // 2. Возвращает студентов со средним баллом >= minAverageGrade
     public IEnumerable<Student> GetStudentsWithMinAverageGrade(double minAverageGrade) => _students.Where(student => student.Grades.Average() >= minAverageGrade);
 
-    // 3. Возвращает студентов, отсортированных по имени (A-Z)
     public IEnumerable<Student> GetStudentsOrderedByName()
         => _students.OrderBy(student => student.Name);
 
-    // 4. Группировка по факультету
     public ILookup<string, Student> GroupStudentsByFaculty()
         => _students.ToLookup(student => student.Faculty);
 
-    // 5. Находит факультет с максимальным средним баллом
     public string GetFacultyWithHighestAverageGrade()
         => _students.GroupBy(student => student.Faculty)
                     .Select(group => new
